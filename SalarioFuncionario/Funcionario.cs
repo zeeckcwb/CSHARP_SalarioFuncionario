7bae80e08f8ebd3f81421cf8eb480b2129bf6bcf
zeeckcwb <ezqlcwb@hotmail.com>
2021-03-05 14:14:53 -0300

    First upload

---
+++ SalarioFuncionario/Funcionario.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Globalization;
+
+namespace SalarioFuncionario
+{
+    class Funcionario
+    {
+        public string Nome;
+        public double SalarioBruto;
+        public double Imposto;
+
+        public double SalarioLiquido()
+        {
+            return SalarioBruto - Imposto;
+        }
+
+        public void AumentarSalario(double porcentagem)
+        {
+            SalarioBruto = SalarioBruto + (SalarioBruto * porcentagem) / 100.0;
+        }
+
+        public override string ToString()
+        {
+            return $"{Nome}, R${SalarioLiquido().ToString("F2", CultureInfo.InvariantCulture)}";
+        }
+    }
+}
---
+++ SalarioFuncionario/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Globalization;
+
+namespace SalarioFuncionario
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Funcionario func = new Funcionario();
+
+            Console.WriteLine("Entre com os seus dados: ");
+            Console.Write("Nome: ");
+            func.Nome = Console.ReadLine();
+            Console.Write("Salário Bruto: ");
+            func.SalarioBruto = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Console.Write("Imposto: ");
+            func.Imposto = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Console.WriteLine();
+            Console.WriteLine($"Funcionário: {func}");
+            Console.WriteLine();
+            Console.Write("Digite a porcentagem para aumentar o salário: ");
+            double porcentagem = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            func.AumentarSalario(porcentagem);
+            Console.WriteLine();
+            Console.WriteLine($"Dados Atualizados: {func}");
+
+        }
+    }
+}
